daba89fbd8ccf202b5b891c377efab33c4487045
vangarilokesh <98004966+vangarilokesh@users.noreply.github.com>
2022-11-19 11:02:26 +0530

    first committo maven java project

---
+++ src/main/java/SEGroup/MavenJavaProject656/App.java
@@ -0,0 +1,13 @@
+package SEGroup.MavenJavaProject656;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from Lokesh 20bd1a6656! Maven Java Project" );
+    }
+}
